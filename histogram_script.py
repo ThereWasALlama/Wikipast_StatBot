7b39a4e7d2d1501f9c700e32318114b7776eaa68
Nicholas Greensmith <LlamaGod@tsf-436-wpa-4-116.epfl.ch>
2019-05-07 15:20:54 +0200

    removed useless file

--- histogram_script.py
+++
@@ -1,7 +0,0 @@
-# Run the bot to generate the new histogram plot
-
-from save_all_birthdata import save_all_birthdata
-from  birth_histogram import birth_histogram
-
-save_all_birthdata()
-birth_histogram()
